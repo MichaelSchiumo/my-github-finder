b17689877e7e1f6cc13d0adcb523be2444b81c6c
Michael Schiumo <mschiumo18@gmail.com>
2020-05-22 17:03:17 -0400

    changes

--- Desktop/github-finder/src/App.js
+++ Desktop/github-finder/src/App.js
@@ -5,12 +5,18 @@ import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search.js';
 import Alert from './components/layout/Alert';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import About from './components/pages/About';
+import User from './components/users/User';
 
 class App extends React.Component {
   state = {
     users: [],
     loading: false,
     alert: null,
+    user: {},
+    repos: [],
   };
 
   async componentDidMount() {
@@ -39,6 +45,31 @@ class App extends React.Component {
     });
   };
 
+  //get single user
+  getUser = async (username) => {
+    this.setState({
+      loading: true,
+    });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
+  getUserRepos = async (username) => {
+    this.setState({
+      loading: true,
+    });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   //clear users from state
   clearUsers = () =>
     this.setState({
@@ -62,22 +93,50 @@ class App extends React.Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user, repos } = this.state;
 
     return (
-      <div className='App'>
-        <NavBar />
-        <div className='container'>
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <NavBar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                    getUserRepos={this.getUserRepos}
+                    repos={repos}
+                  />
+                )}
+              />
+              }
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ Desktop/github-finder/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+export const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>App to Search Github Users</p>
+      <p>Version 1.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
